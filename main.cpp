da086e8f5cbf1325525089c17b3dd2be20a6c647
zj <zj@webturing.com>
2018-06-21 11:25:22 +0800

    runtime configure

--- main.cpp
+++ main.cpp
@@ -6,14 +6,15 @@ typedef long long LL;
 
 string fmt(const double exp, int scale = 6) {
     ostringstream os;
-    os << setiosflags(ios::fixed) << exp;
+    os << setiosflags(ios::fixed) << setprecision(scale)
+    exp;
     return os.str();
 }
 
 #define LOCAL
 
 int main(int argc, char const *argv[]) {
-    std::ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
 #ifdef LOCAL
     ifstream cin("input.txt");
     assert(cin);
@@ -22,7 +23,7 @@ int main(int argc, char const *argv[]) {
 
 #ifdef LOCAL
     LL end = clock();
-    cerr << "Elapsed " << fmt((double) (end - start) / CLOCKS_PER_SEC) << endl;
+    cout << "Elapsed " << fmt((double) (end - start) / CLOCKS_PER_SEC) << endl;
 #endif
     return 0;
 }
