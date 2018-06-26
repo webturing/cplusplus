f9284ba93c41d355b35fe2b8776f1cc429d1f0b0
jal <517486222@qq.com>
2018-06-27 07:44:44 +0800

    临时提交

--- main.cpp
+++ main.cpp
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int>v;
-void dfs(int n){
-    if(n == 0){
-        if (v.size() <=1)
-            return;
-        cout<<*v.begin();
-        for(int i = 1; i < v.size(); i++){
-            cout << "+"<<v[i];
-        }
-        cout << endl;
-        return;
-    }
-    int left = 1;
-    if(v.size()>0){
-        left = v.back();
-    }
-    for(int i = left; i<=n;i++){
-        {
-            v.push_back(i);
-            dfs( n - i);
-            v.pop_back();
+int solution(vector<int>v, int left, int right){
+    int ret = 0;
+    for(int i = left; i < right; i++){
+        switch (i){
+            case 0:ret += 5;
+                break;
+            case 2:ret += 11;
+                break;
+            case 3:ret += 7;
+                break;
+            case 4:ret += -1;
         }
     }
+    ret += v[right];
+    if(right == 2)
+    return ret;
 }
 int main(){
     ifstream cin("input.txt");
     int n;
     cin >> n;
-    dfs(n);
+    vector<int>v;
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        if(x != 1){
+            v.push_back(x);
+        }
+    }
+    int m = 0;
+    for(int i = 0; i < v.size(); i++){
+        for(int j = i; j < v.size(); j++){
+            int sum = solution(v,i,j);
+            m = max(sum, m);
+        }
+    }
+    cout << m << endl;
 }
