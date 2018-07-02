ddf0661277f3b7656b46dd45bf7b1f0bc1c11fe8
zj <zj@webturing.com>
2018-07-03 00:00:01 +0800

    筛子模拟 周期性 改进了代码结构 持续改进代码~~

--- oj.noi.cn/1158Doss.cpp
+++ oj.noi.cn/1158Doss.cpp
@@ -8,20 +8,17 @@ struct Doss {
     Doss() { up = 1, down = 6, left = 4, right = 3, front = 2, back = 5; }
 
     void moveLeftToRight() {
-        int up, down, left, right, front, back;
-        up = this->left, down = this->right, right = this->up, left = this->down;
+        int up = this->left, down = this->right, right = this->up, left = this->down;
         this->up = up, this->down = down, this->right = right, this->left = left;
     }
 
     void moveRightToLeft() {
-        int up, down, left, right, front, back;
-        up = this->right, down = this->left, left = this->up, right = this->down;
+        int up = this->right, down = this->left, left = this->up, right = this->down;
         this->up = up, this->down = down, this->right = right, this->left = left;
     }
 
     void moveUpToDown() {
-        int up, down, left, right, front, back;
-        down = this->front, front = this->up, up = this->back, back = this->down;
+        int down = this->front, front = this->up, up = this->back, back = this->down;
         this->down = down, this->front = front, this->up = up, this->back = back;
     }
 };
@@ -33,14 +30,9 @@ int main() {
     long long up = doss.up;
     for (int i = 0; i < r; i++) {
         up += (c - 1) / 4 * 14;
-        if (i & 1)
-            for (int j = 0; j < (c - 1) % 4; j++)doss.moveRightToLeft(), up += doss.up;
-        else
-            for (int j = 0; j < (c - 1) % 4; j++)doss.moveLeftToRight(), up += doss.up;
-        if (i < r - 1) {
-            doss.moveUpToDown();
-            up += doss.up;
-        }
+        if (i & 1) for (int j = 0; j < (c - 1) % 4; j++)doss.moveRightToLeft(), up += doss.up;
+        else for (int j = 0; j < (c - 1) % 4; j++)doss.moveLeftToRight(), up += doss.up;
+        if (i < r - 1) doss.moveUpToDown(), up += doss.up;
     }
     cout << up << endl;
     return 0;
