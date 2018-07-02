89f958d9cf8c08f5a1fbf7d2ce7e1a64ee4f25a7
Cathy <30455110+2604150210@users.noreply.github.com>
2018-07-02 10:16:59 +0800

    数字划分2，还是组合数，不过要先给每个盘子补一个苹果，Q[m+n-1][n-1]这就变成了数字划分1了

---
+++ oj.noi.cn/1186_number_division_2.cpp
@@ -0,0 +1,34 @@
+//
+// Created by ubuntu on 18-7-2.
+//
+
+//
+// Created by ubuntu on 18-7-2.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int maxn = 60 + 1;
+typedef unsigned long long LL;
+LL Q[maxn][maxn];
+
+void fill() {
+    for (int i = 0; i < maxn; i++) {
+        Q[i][0] = Q[i][i] = 1;
+    }
+    for (int i = 2; i < maxn; i++) {
+        for (int j = 1; j < i; j++) {
+            Q[i][j] = Q[i - 1][j] + Q[i - 1][j - 1];
+        }
+    }
+}
+
+int main() {
+    ifstream cin("input.txt");
+    int n, m;
+    cin >> n >> m;
+    fill();
+    cout << Q[m + n - 1][n - 1] << endl;
+    return 0;
+}
