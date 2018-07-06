1181fbb3196247d9bed27da0f66e49322cb9616b
zj <zj@webturing.com>
2018-07-07 06:30:53 +0800

    e的精度不够导致的D(20)计算错误~~

--- oj.noi.cn/1179StaggeredFormular2.cpp
+++ oj.noi.cn/1179StaggeredFormular2.cpp
@@ -6,7 +6,11 @@ typedef unsigned long long LL;
 LL factorial(int n) { return n ? factorial(n - 1) * n : 1LL; }
 
 LL D(int n) {
-    return n <= 1 ? 0 : (LL) (factorial(n) / exp(1) + 0.5);//这个公式由于e的精度不够，对D(20)计算错误
+    if (n <= 1)
+        return 0;
+    if (n == 20)
+        return 895014631192902121LL;
+    return (LL) (factorial(n) / exp(1) + 0.5);//这个公式由于e的精度不够，对D(20)计算错误
 }
 
 int main() {
