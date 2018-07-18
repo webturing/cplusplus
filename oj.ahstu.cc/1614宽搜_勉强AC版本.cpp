9b8a485ad0a3b3d56fb2f22a57b8fb00393f3c09
zj <zj@webturing.com>
2018-07-18 16:33:34 +0800

    1614宽搜_勉强AC版本.cpp

--- oj.ahstu.cc/1614宽搜_勉强AC版本.cpp
+++ oj.ahstu.cc/1614宽搜_勉强AC版本.cpp
@@ -5,7 +5,7 @@ string init;
 string target;
 queue<pair<string, int> > Q;
 set<string> history;
-int success[4][2] = {0, 1, 0, -1, 1, 0, -1, 0};
+int succ[4][2] = {0, 1, 0, -1, 1, 0, -1, 0};
 
 int bfs() {
     while (Q.size()) {
@@ -13,22 +13,22 @@ int bfs() {
             return Q.front().second;
         }
         string str = Q.front().first;
+        int depth = Q.front().second;
+        history.insert(Q.front().first);
+        Q.pop();
         int pos = str.find('.');
         int x = pos / 3;
         int y = pos % 3;
         for (int i = 0; i < 4; i++) {
-            int x0 = x + success[i][0];
-            int y0 = y + success[i][1];
+            int x0 = x + succ[i][0];
+            int y0 = y + succ[i][1];
             if (x0 < 0 || y0 < 0 || x0 > 2 || y0 > 2)continue;
             string temp = str;
             swap(temp[pos], temp[3 * x0 + y0]);
             if (history.find(temp) != history.end())
                 continue;
-            Q.push(make_pair(temp, Q.front().second + 1));
-
+            Q.push(make_pair(temp, depth + 1));
         }
-        history.insert(Q.front().first);
-        Q.pop();
     }
     return -1;
 }
