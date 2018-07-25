c89e60a91349681337f3d11869e8a14d5a9fe279
jal <517486222@qq.com>
2018-07-25 14:14:49 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,24 +1,107 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef unsigned long long LL;
-LL f[31][2][2] = {0};
 
-int main() {
-    int n;
-    cin >> n;
-    if (n < 3) {
-        cout << 0 << endl;
-        return 0;
+int n,x,y;
+
+struct Fraction{
+    int up,down;
+    Fraction operator+(Fraction that){
+        Fraction ret;
+        ret.down = down * that.down;
+        ret.up = down * that.up + up * that.down;
+    }
+};
+int getMinSwaps(vector<int> &nums){
+    //排序
+    vector<int> nums1(nums);
+    sort(nums1.begin(),nums1.end());
+    unordered_map<int,int> m;
+    int len = nums.size();
+    for (int i = 0; i < len; i++){
+        m[nums1[i]] = i;//建立每个元素与其应放位置的映射关系
     }
 
-    f[3][0][0] = f[3][0][1] = f[3][1][0] = 2, f[3][1][1] = 1;
-    for (int i = 4; i <= n; i++) {
-        f[i][0][0] = f[i - 1][0][0] + f[i - 1][0][1];
-        f[i][0][1] = f[i - 1][1][1] + f[i - 1][1][0];
-        f[i][1][0] = f[i - 1][0][0] + f[i - 1][0][1];
-        f[i][1][1] = f[i - 1][1][0];
+    int loops = 0;//循环节个数
+    vector<bool> flag(len,false);
+    //找出循环节的个数
+    for (int i = 0; i < len; i++){
+        if (!flag[i]){//已经访问过的位置不再访问
+            int j = i;
+            while (!flag[j]){
+                flag[j] = true;
+                j = m[nums[j]];//原序列中j位置的元素在有序序列中的位置
+            }
+            loops++;
+        }
     }
-    cout << (1LL << n) - (f[n][0][0] + f[n][0][1] + f[n][1][0] + f[n][1][1]) << endl;
-    return 0;
+    return len - loops;
+}
+
+int cnt;
+int arr[1005];
+void Merge(int* arr,int* tmp,int left,int right,int rightEnd){
+    int leftEnd = right - 1;
+    int start = left;
+    while (left <= leftEnd && right <= rightEnd){
+        if (arr[left] > arr[right]){
+            tmp[start++] = arr[right++];
+            cnt += (leftEnd - left+1); //如果当前left位置上的数大于right位置上的数，那么从left到leftEnd所有的数都大于
+        }
+        else{
+            tmp[start++] = arr[left++];
+        }
+    }
+    while (left <= leftEnd){
+        tmp[start++] = arr[left++];
+    }
+    while (right <= rightEnd){
+        tmp[start++] = arr[right++];
+    }
+}
+void MergeSort(int* arr,int* tmp,int n,int length)//length当前有序子列的长度
+{
+    int i;
+    for (i = 0; i <= n - 2 * length; i += 2 * length){
+        Merge(arr,tmp,i,i+length,i+2*length-1);
+    }
+    //最后剩下两个子列，进行归并
+    if (i + length < n){
+        Merge(arr,tmp,i,i+length,n-1);
+    }
+    else{//只剩最后一个子列，不能成对
+        for (int j = i; j < n; j++){
+            tmp[j] = arr[j];
+        }
+    }
+}
+void Merge_Sort(int* arr,int n){
+    int lenght = 1;
+    int* tmp = (int *)malloc(sizeof(int)*n);
+    while (lenght < n){
+        MergeSort(arr,tmp,n,lenght);
+        lenght *= 2;
+        MergeSort(tmp,arr,n,lenght);
+        lenght *= 2;
+    }
+    free(tmp);
+}
+int main(){
+    while(cin >> n >> x >> y){
+        vector<int>v(n);
+        for(int i = 0; i < n; i++){
+            cin >> v[i];
+            arr[i] = v[i];
+        }
+        int first = getMinSwaps(v);
+        cout << "first:"<<first << endl;
+        first *= x;
+        cnt=0;
+        Merge_Sort(arr,n);
+        int second = cnt;
+        cout << "second:"<<second << endl;
+        second *= y;
+        cout << min(first, second) << endl;
+    }
+
 }
