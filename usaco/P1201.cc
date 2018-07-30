66a9bf4d146a8daecc1cbb9c1392306beab96a2c
zj <zj@webturing.com>
2018-07-30 21:14:21 +0800

    USACO 002

---
+++ usaco/P1201.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int np;
+  cin >> np;
+  vector<string> persons(np);
+  map<string, int> mp;
+  for (auto& person : persons) {
+    cin >> person;
+    mp[person] = 0;
+  }
+  for (int i = 0; i < np; i++) {
+    string sender;
+    int amount, ng;
+    cin >> sender >> amount >> ng;
+    if (amount == 0 && ng == 0) continue;
+    // mp[sender] -= amount;
+    amount /= ng;
+    while (ng--) {
+      string reciever;
+      cin >> reciever;
+      mp[reciever] += amount;
+      mp[sender] -= amount;
+    }
+  }
+  for (auto person : persons) cout << person << " " << mp[person] << endl;
+  return EXIT_SUCCESS;
+}
