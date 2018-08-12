a99a71a56e468460d8fca7661810f7f027d5b251
zj <zj@webturing.com>
2018-08-12 14:01:43 +0800

    baidu contest(first B)

---
+++ baiduStar2018B/D.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, b) for (int i = (a); i <= (b); i++)
+#define per(i, a, b) for (int i = (a); i >= (b); i--)
+typedef long long LL;
+
+#define LOCAL
+
+int main() {
+#ifdef LOCAL
+  ifstream cin("D.in");
+  ofstream cout("D.out");
+  LL start = clock();
+#endif
+  ios::sync_with_stdio(false);
+
+#ifdef LOCAL
+  LL end = clock();
+  cerr << 1.0 * (end - start) / CLOCKS_PER_SEC << endl;
+#endif
+  return 0;
+}
