6e5945ff7d557ff9881dad5b6482a346e9d408fd
zj <zj@webturing.com>
2018-09-12 20:20:05 +0800

    Programming I

---
+++ judge.u-aizu.ac.jp/Introduction to Programming I/ITP1_2_B_Range.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  if (a < b && b < c)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+  return 0;
+}
