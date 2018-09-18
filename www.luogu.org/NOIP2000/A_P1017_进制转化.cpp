066c408419cdd213c1ff56264a0b47dca0b42408
jal <517486222@qq.com>
2018-09-18 12:43:47 +0800

    负进制数转换，要重写除法运算和模运算

--- www.luogu.org/NOIP2000/A_P1017_进制转化.cpp
+++ www.luogu.org/NOIP2000/A_P1017_进制转化.cpp
@@ -1,14 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 string D = "0123456789ABCDEFGHIJ";
-int main(int argc, char const *argv[]) {
-  int n, r;
-  cin >> n >> r;
-  vector<int> R(1);
-  int t = abs(r);
-  while (t < n) {
-        R.push_back(t*abs(r)));
-  }
 
+pair<int, int> div_mod(int n, int r) {
+    if (n >= 0) {
+        return make_pair(n / r, n % r);
+    }
+    if (n % r == 0) {
+        return make_pair(n / r, 0);
+    }
+
+    if (r < 0) {
+        return make_pair(n / r + 1, n % r - r);
+    }
+    return make_pair(-n / r + 1, n % r - r);
+}
+int main(int argc, char const *argv[]) {
+    int n, base;
+    cin >> n >> base;
+    int n1 = n;
+    vector<int> v;
+    int r = base;
+    while (n) {
+        pair<int, int> res = div_mod(n, r);
+        v.push_back(res.second);
+        n = res.first;
+  }
+    reverse(v.begin(), v.end());
+    cout << n1 << "=";
+    for (auto item:v) {
+        cout << D[item];
+    }
+    cout << "(base" << base << ")" << endl;
   return 0;
 }
