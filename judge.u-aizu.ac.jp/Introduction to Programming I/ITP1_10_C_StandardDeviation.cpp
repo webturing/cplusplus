d25679cd2cbb49d236e0eb55b34dd5ada3555e11
webturing <zj@webturing.com>
2019-04-28 15:39:45 +0800

    完善 C++ Programming I

--- judge.u-aizu.ac.jp/Introduction to Programming I/ITP1_10_C_StandardDeviation.cpp
+++ judge.u-aizu.ac.jp/Introduction to Programming I/ITP1_10_C_StandardDeviation.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int main() {
-    int n;
-    while (cin >> n) {
-        if (n == 0) break;
+    for (int n; cin >> n and n != 0;) {
         vector<double> S(n);
-        for (int i = 0; i < n; i++) cin >> S[i];
-        double m = accumulate(S.begin(), S.end(), 0);
+        for (auto &e:S)cin >> e;
+        double m = accumulate(S.begin(), S.end(), 0.0);
         double sd = 0;
-        for (int i = 0; i < n; i++) sd += (n * S[i] - m) * (n * S[i] - m);
+        for (auto e:S) sd += (n * e - m) * (n * e - m);
         cout << fixed << setprecision(8) << sqrt(sd / n) / n << endl;
     }
+
     return 0;
 }
