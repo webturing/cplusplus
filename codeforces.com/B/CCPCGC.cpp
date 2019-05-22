4dc11c466928f5aba0f96834c96e6ec0b946d0fa
webturing <zj@webturing.com>
2019-05-22 13:43:22 +0800

    二分查找，排序，字符串

--- codeforces.com/B/CCPCGC.cpp
+++ codeforces.com/B/CCPCGC.cpp
@@ -5,28 +5,27 @@ using namespace std;
 typedef long long LL;
 
 int main() {
+    ifstream cin("input.txt");
     int n;
     cin >> n;
-    vector<int> a;
-    for (int i = 0; i < n; i++) {
-        int t;
-        cin >> t;
-        a.push_back(t);
-    }
-
+    vector<int> a(n);
+    for (auto &e:a)cin >> e, e = abs(e);
     sort(a.begin(), a.end());
-
-    double cnt = 0;
-
+    LL cnt = 0;
     for (int i = 0; i < n; i++) {
-        int x = abs(a[i]);
-        auto left = lower_bound(a.begin(), a.end(), x * -2);
-        auto right = upper_bound(a.begin(), a.end(), x * 2 );
-        cnt += abs(right - left)/2;
-        if (a[i] < *right and a[i] >= *left)--cnt;
-
+        int &x = a[i];
+        if (x > 0) {//2x>=y>=x
+            auto left = lower_bound(a.begin() + i + 1, a.end(), x );
+            auto right = upper_bound(a.begin() + i + 1, a.end(), 2 * x);
+            cnt += right - left;
+        } else if (x < 0) {//2*abs(x)>=y>=x/2
+            auto left = lower_bound(a.begin() + i + 1, a.end(), x / 2);
+            auto right = upper_bound(a.begin() + i + 1, a.end(), 2 * -x);
+            cnt += right - left;
+        }
     }
-    cout << cnt  << endl;
+
+    cout << cnt << endl;
     return 0;
 }
-}
+
