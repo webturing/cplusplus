d2922f459b73bb494483a8c32fc64126eaf5fc66
webturing <zj@webturing.com>
2019-05-25 16:23:53 +0800

    XOR在字符串上的模拟，read<T>实现很实用

---
+++ codeforces.com/61A - Ultra-Fast Mathematician.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef unsigned long long LL;
+
+template<typename T>
+inline void oo(string str, T val) { cerr << str << val << endl; }
+
+template<typename T>
+inline T read() {
+    T x;
+    cin >> x;
+    return x;
+}
+
+#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
+#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
+
+int main() {
+    auto a = read<string>();
+    auto b = read<string>();
+    string c(a.size(), '0');
+    FOR(i, 0, a.size())if (a[i] != b[i])++c[i];
+    cout << c << endl;
+    return 0;
+}
