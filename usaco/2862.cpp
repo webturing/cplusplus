39f71e55e5ca72758bb9b4d8f09c9ee216a1eeff
webturing <zj@webturing.com>
2019-06-23 22:58:21 +0800

    USACO  1.1.1 Your Ride Is Here 字符串散列

--- usaco/2862.cpp
+++ usaco/2862.cpp
@@ -6,8 +6,8 @@
 
 using LL=long long;
 
-#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
-#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
+#define FOR(i, x, y) for (std::decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
+#define FORD(i, x, y) for (std::decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
 
 
 int hash(std::string s) {
@@ -18,6 +18,8 @@ int hash(std::string s) {
 
 int main() {
     std::string a, b;
+    a.resize(100);
+    b.resize(100);
     scanf("%s%s", &a[0], &b[0]);
     puts(hash(a) == hash(b) ? "GO" : "STAY");
     return EXIT_SUCCESS;
