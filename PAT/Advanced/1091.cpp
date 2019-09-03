a64c703151d570d54a4c404fb4b5e297499a377a
Cathy <517486222@qq.com>
2019-09-03 12:30:29 +0800

    三维bfs岛屿搜索问题，使用dfs栈溢出导致段错误，改为bfs即可

--- PAT/Advanced/1091.cpp
+++ PAT/Advanced/1091.cpp
@@ -1,9 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-int cnt = 0, sum = 0;
-const int L = 61, N = 1290, M = 130;
-int n, m, l, t;
-int G[L][N][M], book[L][N][M];
 int next_dir[6][3] = {
 	{0, 0, -1},
 	{0, 0, 1},
@@ -12,32 +8,47 @@ int next_dir[6][3] = {
 	{-1, 0, 0},
 	{1, 0, 0},
 };
-void dfs(int x, int y, int z){
-	sum++;
-	book[x][y][z] = 1;
-	for(int i = 0; i < 6; i++){
-		int tx = x + next_dir[i][0];
-		int ty = y + next_dir[i][1];
-		int tz = z + next_dir[i][2];
-		if(tx < 0 || ty < 0 || tz < 0 || tx >= l || ty >= n || tz >= m || G[tx][ty][tz]==0||book[tx][ty][tz])continue;
-		dfs(tx, ty, tz);
-	}
-}
+struct Node
+{
+	int x, y, z;
+};
 int main(){
+	int n, m, l, t;
 	scanf("%d %d %d %d", &n, &m, &l, &t);
+	vector<vector<vector<int>>>G(l), book(l);
 	for(int i = 0; i < l; i++){
+		G[i].resize(n);
+		book[i].resize(n);
 		for(int j = 0; j < n; j++){
+			G[i][j].resize(m);
+			book[i][j].resize(m);
 			for(int k = 0; k < m; k++){
 				scanf("%d", &G[i][j][k]);
 			}
 		}
 	}
+	int cnt = 0;
 	for(int i = 0; i < l; i++){
 		for(int j = 0; j < n; j++){
 			for(int k = 0; k < m; k++){
 				if(book[i][j][k] || G[i][j][k] == 0)continue;
-				sum = 0;
-				dfs(i, j, k);
+				int sum = 0;
+				queue<Node>Q;
+				Q.push({i, j, k});
+				book[i][j][k] = 1;
+				while(Q.size()){
+					Node head = Q.front();
+					Q.pop();
+					sum++;
+					for(int p = 0; p < 6; p++){
+						int tx = head.x + next_dir[p][0];
+						int ty = head.y + next_dir[p][1];
+						int tz = head.z + next_dir[p][2];
+						if(tx < 0 || ty < 0 || tz < 0 || tx >= l || ty >= n || tz >= m || book[tx][ty][tz] || !G[tx][ty][tz])continue;
+						Q.push({tx, ty, tz});
+						book[tx][ty][tz] = 1;
+					}
+				}
 				if(sum >= t)cnt+=sum;
 			}
 		}
