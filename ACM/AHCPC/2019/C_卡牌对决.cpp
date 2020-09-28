6f01dbb8b3d3f5d5bc1dca1b3c20a7d81a60a75d
webturing <zj@webturing.com>
2020-09-28 22:22:57 +0800

    2019AHCPC solutions~

--- ACM/AHCPC/2019/C_卡牌对决.cpp
+++ ACM/AHCPC/2019/C_卡牌对决.cpp
@@ -1,63 +1,39 @@
-#pragma comment(linker, "/stack:247474112")
-#pragma GCC optimize("Ofast")
-
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
-
-typedef long long LL;
-#define endl '\n'
-#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
-#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
-
-template<typename T>
-void OOM(T a, string s = "") {
-    cerr << s << ":";
-    for (auto e:a) cerr << e.first << "," << e.second << " ";
-    cerr << endl;
-}
-
-template<typename T>
-void OO(T a, string s = "") {
-    cerr << s << ":";
-    for (auto e:a)cerr << e << " ";
-    cerr << endl;
-}
-
-template<typename T>
-inline void oo(string str, T val) { cerr << str << val << endl; }
-
-template<typename T>
-inline T read() {
-    T x;
-    cin >> x;
-    return x;
-}
+int n;
+int a[50004];
+int b[50004], p, pn;
+int f[100005];
 
 int main() {
-    std::iostream::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-    int n(read<int>()), m = n * 2;
-    vector<int> A, B;
-    map<int, bool> M;
-    FOR(i, 1, m + 1)M[i] = true;
-    FOR(i, 0, n) {
-        int x(read<int>());
-        B.push_back(x);
-        M[x] = false;
+    scanf("%d", &n);
+    int x;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", a + i);
+        if (i < n / 2) f[a[i]] = 1;
+        else f[a[i]] = 2;
     }
-    FOR(i, 1, m + 1)if (M[i])A.push_back(i);
-
-    sort(B.begin(), B.begin() + n / 2);
-    sort(B.begin() + n / 2, B.end());
-    sort(A.begin(), A.end(), greater<int>());
-    reverse(A.begin(), A.begin() + n / 2);
-    reverse(A.begin() + n / 2, A.end());
-
-    int win = 0;
-    FOR(i, 0, n)if (i < n / 2 and A[i] > B[i] or i >= n / 2 and A[i] < B[i])++win;
-    cout << win << endl;
+    int sum = 0;
+    for (int i = 2 * n; i > 0; i--) {
+        if (f[i] == 0) b[pn++] = i;
+        else if (f[i] == 1) {
+            if (p < pn) {
+                sum++;
+                f[b[p++]] = -1;
+            }
+        } else continue;
+    }
+    p = pn = 0;
+    for (int i = 1; i <= 2 * n; i++) {
+        if (f[i] == 0) b[pn++] = i;
+        else if (f[i] == 2) {
+            if (p < pn) {
+                sum++;
+                f[b[p++]] = -1;
+            }
+        } else continue;
+    }
+    cout << sum;
     return 0;
 }
--- ACM/AHCPC/2019/D.cpp
+++ ACM/AHCPC/2019/D.cpp
@@ -1,171 +1,109 @@
-//
-// Created by zj on 19-5-31.
-//
-
-#pragma comment(linker, "/stack:247474112")
-#pragma GCC optimize("Ofast")
-
-#include <bits/stdc++.h>
-
+#include"bits/stdc++.h"
 using namespace std;
-
-#define endl '\n'
-typedef long long LL;
-
-template<typename T>
-void OOM(T a, string s = "") {
-    cerr << s << ":\t";
-    for (auto e:a) {
-        cerr << e.first << "," << e.second << " ";
-    }
-    cerr << endl;
-}
-
-template<typename T>
-void OO(T a, string s = "") {
-    cerr << s << ":\t";
-    for (auto e:a) {
-        cerr << e << " ";
-    }
-    cerr << endl;
-}
-
-template<typename T>
-inline void oo(string str, T val) { cerr << str << val << endl; }
-
-template<typename T>
-inline T read() {
-    T x;
-    cin >> x;
-    return x;
-}
-
-
-#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
-#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
-struct ZJ{
-    int v, p, q;
-    ZJ(int v, int p, int q):v(v), p(p), q(q){}
+struct node {
+    int v, pi, qi;
 };
-void DIJ(int len, int source, int y, vector<vector<ZJ> >&V, vector<int>&dis, int type){
-    vector<bool>book(len+1,false);
-    dis[source] = 0;
-    for(auto i: V[source]){
-        if(type == 0){
-            dis[i.v] = i.p;
-        }else{
-            dis[i.v] = i.q;
-        }
+vector<node> v[10005], v1[10005];
+vector<pair<int, int> > v2[10005];
+
+struct edge {
+    int id, num;
+
+    friend bool operator<(const edge a, const edge b) {
+        return a.num > b.num;
     }
-    OO(dis, "start print dis = ");
-    book[source] = true;
-    for(int i = 1; i < len; i++){
-        cerr << "i = " << i << endl;
-        int w = -1;
-        int minDis = INT_MAX;
-        for(int j = 1; j <= len; j++){
-            if(book[j] || dis[j] >= minDis)continue;
-            w = j;
-            minDis = dis[j];
-        }
-        cerr << "w = " << w << "minDis = " << minDis << endl;
-        book[w] = true;
-        dis[w] = minDis;
-        for(auto j: V[w]){
-            if(book[j.v])continue;
-            if(type == 0){
-                cerr << "change dis = " << dis[w]+j.p<<endl;
-                if(dis[j.v] > dis[w] + j.p){
-                    dis[j.v] = dis[w] + j.p;
-                }
-            }else{
-                if(dis[j.v] > dis[w] + j.q){
-                    dis[j.v] = dis[w] + j.q;
-                }
+};
+
+int n, m;
+const int INF = 0x3f3f3f3f;
+int dis1[10005], dis2[10005], dis3[10005], visit[10005];
+
+void dijk1(int s) {
+    priority_queue<edge> q;
+    memset(visit, 0, sizeof(visit));
+    memset(dis1, INF, sizeof(dis1));
+    dis1[s] = 0;
+    q.push(edge{s, 0});
+    while (!q.empty()) {
+        edge top = q.top();
+        q.pop();
+        if (visit[top.id])continue;
+        visit[top.id] = 1;
+        for (int i = 0; i < v[top.id].size(); i++) {
+            int dv = v[top.id][i].v;
+            int dpi = v[top.id][i].pi;
+            if (dis1[dv] > dis1[top.id] + dpi) {
+                dis1[dv] = dis1[top.id] + dpi;
+                q.push(edge{dv, dis1[dv]});
             }
         }
-        OO(dis, "dis= ");
-        OO(book, "book= ");
     }
 }
-int main(){
-    ifstream cin("input.txt");
-    int n, m;
+
+void dijk2(int s) {
+    priority_queue<edge> q;
+    memset(visit, 0, sizeof(visit));
+    memset(dis2, INF, sizeof(dis2));
+    dis2[s] = 0;
+    q.push(edge{s, 0});
+    while (!q.empty()) {
+        edge top = q.top();
+        q.pop();
+        for (int i = 0; i < v[top.id].size(); i++) {
+            int dv = v[top.id][i].v;
+            int dqi = v[top.id][i].qi;
+            if (dis2[dv] > dis2[top.id] + dqi) {
+                dis2[dv] = dis2[top.id] + dqi;
+                q.push(edge{dv, dis2[dv]});
+            }
+        }
+    }
+}
+
+void dijk3(int s) {
+    priority_queue<edge> q;
+    memset(visit, 0, sizeof(visit));
+    memset(dis3, INF, sizeof(dis3));
+    dis3[s] = 0;
+    q.push(edge{s, 0});
+    while (!q.empty()) {
+        edge top = q.top();
+        q.pop();
+        if (visit[top.id])continue;
+        visit[top.id] = 1;
+        for (int i = 0; i < v2[top.id].size(); i++) {
+            pair<int, int> t = v2[top.id][i];
+            int dv = t.first;
+            int dw = t.second;
+            if (dis3[dv] > dis3[top.id] + dw) {
+                dis3[dv] = dis3[top.id] + dw;
+                q.push(edge{dv, dis3[dv]});
+            }
+        }
+    }
+}
+
+int main() {
     cin >> n >> m;
-    vector< vector<ZJ> > V1(n+1), V2(n+1);//V1是反向图，V2是原图
-    for(int i = 0; i < m; i++){
-        int u, v, p, q;
-        cin >> u >> v >> p >> q;
-        V1[v].emplace_back(ZJ(u, p, q));
-        V2[u].emplace_back(ZJ(v, p, q));
+    for (int i = 1; i <= m; i++) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        v[b].push_back(node{a, c, d});
+        v1[a].push_back(node{b, c, d});
     }
-    for(int i = 1; i <= n; i++){
-        cerr << i <<": ";
-        for(auto j : V1[i]){
-            cerr << j.v << " " << j.p << " " << j.q << endl;
+    dijk1(n);
+    dijk2(n);
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < v1[i].size(); j++) {
+            node t = v1[i][j];
+            if (dis1[i] < dis1[t.v] + t.pi)ans++;
+            if (dis2[i] < dis2[t.v] + t.qi)ans++;
+            v2[i].push_back(make_pair(t.v, ans));
+            ans = 0;
         }
     }
-    vector<int>dis_a(n+1, INT_MAX / 2), dis_hero(n+1, INT_MAX / 2);
-    OO(dis_hero);
-    OO(dis_a);
-    DIJ(n, n, 1, V1, dis_hero, 0);
-    OO(dis_hero, "final dis_hero = ");
-    DIJ(n, n, 1, V1, dis_a, 1);
-    OO(dis_a, "final dis_a = ");
-    vector<int>dis(n+1, INT_MAX/2);
-    for(auto i:V2[1]){
-        dis[i.v] = (dis_hero[i.v] + i.p > dis_hero[1]) + (dis_a[i.v] + i.q > dis_a[1]);
-    }
-    dis[1] = 0;
-    vector<bool>book(n+1, false);
-    book[1] = true;
-    for(int i = 1; i < n; i++){
-        int w = -1, minDis = INT_MAX/2;
-        for(int j = 1; j <= n; j++){
-            if(book[j] == 0 and dis[j] < minDis){
-                w = j;
-                minDis = dis[j];
-            }
-        }
-        cerr << "w = " << w << endl;
-        book[w] = true;
-        for(auto j : V2[w]){
-            if(book[j.v])continue;
-            int t = (dis_hero[j.v] + j.p > dis_hero[w]) + (dis_a[j.v] + j.q > dis_a[w]);
-            if(dis[j.v] > dis[w] + t){
-                dis[j.v] = dis[w] + t;
-            }
-        }
-        OO(dis, "dis = ");
-    }
-    cout << dis[n] << endl;
+    dijk3(1);
+    cout << dis3[n];
     return 0;
 }
-/*
-5 7
-3 4 7 1
-1 3 2 20
-1 4 17 18
-4 5 25 3
-1 2 10 1
-3 5 4 14
-2 4 6 5
-1
-5 7
-3 4 7 11
-1 3 2 20
-1 4 7 18
-4 5 2 3
-1 2 1 10
-3 5 14 4
-2 4 6 5
-0
-4 6
-1 2 13 40
-2 3 7 12
-1 3 15 6
-1 4 11 2
-2 4 14 1
-3 4 29 14
-0
- */
